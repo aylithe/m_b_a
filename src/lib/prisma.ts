bb3401a62cc719dc627c4d93b28c07f134e00d7b
Ayooluwa Adedipe <ayooluwaadedipe@gmail.com>
2026-09-15 23:57:29 +0000

    chore: prisma setup

---
+++ src/lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+
+// Create ONE instance and export it
+export const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === 'development'
+      ? ['query', 'warn', 'error']
+      : ['warn', 'error'],
+});
