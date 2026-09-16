460d24dd45415d841db29f53360b5f9dea8a5550
Ayooluwa Adedipe <ayooluwaadedipe@gmail.com>
2026-09-16 23:27:39 +0000

    chore: refresh token unique token update

--- src/repositories/refresh.token.repository.ts
+++ src/repositories/refresh.token.repository.ts
@@ -4,4 +4,12 @@ export const refreshTokenRepository = {
   async create(data: { userId: string; token: string; expiresAt: Date }) {
     return prisma.refreshToken.create({ data });
   },
+  async findByToken(token: string) {
+    return prisma.refreshToken.findUnique({
+      where: { token },
+    });
+  },
+  async delete(where: { token: string }) {
+    return prisma.refreshToken.deleteMany({ where });
+  },
 };
