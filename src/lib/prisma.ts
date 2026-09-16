17565e30c31e3cf2d33ac3414044f2289504fc4b
Ayooluwa Adedipe <ayooluwaadedipe@gmail.com>
2026-09-16 22:44:23 +0000

    chore: fix prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
 // Create ONE instance and export it
 export const prisma = new PrismaClient({
+  adapter,
   log: process.env.NODE_ENV === 'development' ? ['query', 'warn', 'error'] : ['warn', 'error'],
 });
