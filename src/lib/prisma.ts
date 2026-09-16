d4d4113d4fbb7a0ef28dc09f4a6563bc628d5768
Ayooluwa Adedipe <ayooluwaadedipe@gmail.com>
2026-09-16 22:27:32 +0000

    chore: auth events set up

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,8 +2,5 @@ import { PrismaClient } from '@prisma/client';
 
 // Create ONE instance and export it
 export const prisma = new PrismaClient({
-  log:
-    process.env.NODE_ENV === 'development'
-      ? ['query', 'warn', 'error']
-      : ['warn', 'error'],
+  log: process.env.NODE_ENV === 'development' ? ['query', 'warn', 'error'] : ['warn', 'error'],
 });
