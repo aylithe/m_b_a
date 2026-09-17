bdee242078e14d9200f7195a006951486d7f7e25
Ayooluwa Adedipe <ayooluwaadedipe@gmail.com>
2026-09-17 00:16:32 +0000

    chore: secondary key update

---
+++ prisma/migrations/20260916234033_auto_update/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "RefreshToken_userId_idx" ON "RefreshToken"("userId");
+
+-- CreateIndex
+CREATE INDEX "RefreshToken_expiresAt_idx" ON "RefreshToken"("expiresAt");
+
+-- AddForeignKey
+ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
