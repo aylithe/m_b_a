c5c14bca5102b62090e3d6fea7c08b6f176a0dad
Ayooluwa Adedipe <ayooluwaadedipe@gmail.com>
2026-09-17 00:17:22 +0000

    chore: routes setup

---
+++ src/routes/auth.ts
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+import * as authService from '../services/auth.services';
+
+const router = Router();
+
+router.post('/register', async (req, res, next) => {
+  try {
+    console.log(req.body, 'REQ BODY LOGGG');
+    const user = await authService.register(req.body);
+    res.status(201).json({ user });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const result = await authService.login({
+      ...req.body,
+      deviceInfo: req.headers['user-agent'],
+    });
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/refresh', async (req, res, next) => {
+  try {
+    const result = await authService.refresh(req.body.refreshToken);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/logout', async (req, res, next) => {
+  try {
+    await authService.logout(req.body.refreshToken);
+    res.json({ message: 'Logged out' });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
