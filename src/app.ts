2270cf4d9de7c1fab7c268db1fefe1eaffb6bf31
Ayooluwa Adedipe <ayooluwaadedipe@gmail.com>
2026-09-17 00:17:40 +0000

    chore: use dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config';
 import express, { Request, Response } from 'express';
+import authRoutes from './routes/auth';
 import './events/auth.events';
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Hello, World!');
@@ -11,3 +16,5 @@ app.get('/', (_req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+app.use('/api/auth', authRoutes);
